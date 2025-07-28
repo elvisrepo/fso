c0cfbd1e5e5f81615a6a29a380e282c9cc1c2e52
elvisrepo <elvis.dev2@gmail.com>
2025-07-28 18:51:56 +0200

    init

---
+++ introdemo/src/App.jsx
@@ -0,0 +1,34 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
+
+const App = () => {
+
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
+
+  return (
+    <div>
+      <p>{friends[0].name}</p>
+      <p>{friends[1].name}</p>
+    </div>
+  )
+}
+
+export default App

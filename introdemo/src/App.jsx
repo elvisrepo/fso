3d38fd54beef11a9937e92835acf63e97b985c8f
elvisrepo <elvis.dev2@gmail.com>
2025-07-29 18:35:24 +0200

    part1.d

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,34 +1,28 @@
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
-}
+import { useState } from "react"
+const Display = props => <div>{props.value}</div>
 
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
-    </div>
-  )
-}
-
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
+  const [value, setValue] = useState(10)
 
-  const friends = [
-    { name: 'Peter', age: 4 },
-    { name: 'Maya', age: 10 },
-  ]
-
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
   return (
     <div>
-      <p>{friends[0].name}</p>
-      <p>{friends[1].name}</p>
+      <Display value={value} />
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
+
 
 export default App

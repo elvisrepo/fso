616af28182e40e681e9a7a32b75d4b1e3bfba204
elvisrepo <elvis.dev2@gmail.com>
2025-07-30 15:46:08 +0200

    anecdotes 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -13,10 +13,52 @@ const App = () => {
   ]
 
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+  console.log(votes)
+
+  // on CLick random anecdote selected
+  // get random num within anecdotes length, setSelected
+
+  const nextHandler = () => {
+    //between 0 and anecdotes len - 1
+    const anec_length = anecdotes.length
+    const rand = Math.floor(Math.random() * (anec_length))
+
+    setSelected(rand)
+  }
+
+  ///   ancedote - vote
+  const voteHandler = () => {
+    // add to the votes count of the current index of selected
+    const copy_Votes = [...votes]
+    copy_Votes[selected] += 1
+    setVotes(copy_Votes)
+  }
+
+  // iterate over the votes arr, find the most voted value, return its index
+
+  let mostVotedIndex = 0
+  let mostVotedValue = votes[0]
+
+  votes.forEach((element, index) => {
+    if (element > mostVotedValue) {
+      mostVotedValue = element
+      mostVotedIndex = index
+    }
+  });
+
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
+      <button onClick={nextHandler}>  Next anecdote</button>
+      <button onClick={voteHandler}>  Vote</button>
+
+
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[mostVotedIndex]}
     </div>
   )
 }

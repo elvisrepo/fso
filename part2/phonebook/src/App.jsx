7a336ec2a492b6269c7fde7687a4c351fbf22163
elvisrepo <elvis.dev2@gmail.com>
2025-08-02 14:42:22 +0200

    2.9 filtering

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterSearch, setFilterSearch] = useState('')
 
   //add function
   const addPerson = (e) => {
@@ -45,9 +46,29 @@ const App = () => {
 
   }
 
+  const handleFilterSearch = (e) => {
+
+    setFilterSearch(e.target.value)
+
+  }
+
+  // filter persons list
+  const filteredPersons = persons.filter(per => per.name.toLowerCase().includes(filterSearch.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+
+      <div>
+        filter shown with
+        <input value={filterSearch}
+          onChange={handleFilterSearch}
+        >
+
+        </input>
+      </div>
+
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName}
@@ -65,7 +86,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
 
-      {persons.map(per => <p key={per.name}>{per.name} {per.number}</p>)}
+      {filteredPersons.map(per => <p key={per.name}>{per.name} {per.number}</p>)}
 
     </div>
   )

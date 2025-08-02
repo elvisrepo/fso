8212ccf97c31720ab2dacf50d07c746b7dca3541
elvisrepo <elvis.dev2@gmail.com>
2025-08-02 13:36:34 +0200

    2.6 phonebook

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  //add function
+  const addPerson = (e) => {
+    e.preventDefault()
+
+    const newPerson = {
+      name: newName
+    }
+
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  // onChange event handler
+  const handleNameChange = (e) => {
+
+    setNewName(e.target.value)
+
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+
+      {persons.map(per => <p key={per.name}>{per.name}</p>)}
+
+    </div>
+  )
+}
+
+export default App

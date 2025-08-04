10642e2203f42919a1124d0727f70f8a6edea68d
elvisrepo <elvis.dev2@gmail.com>
2025-08-04 13:54:00 +0200

    post note

--- node_test/index.js
+++ node_test/index.js
@@ -1,7 +1,7 @@
-const express =  require('express')
+const express = require('express')
 const app = express()
 
-
+app.use(express.json())
 
 let notes = [
   {
@@ -25,14 +25,14 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request,response) => {
-    response.json(notes)
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
 })
 
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
   const note = notes.find(note => note.id === id)
-  
+
   if (note) {
     response.json(note)
   } else {
@@ -45,6 +45,33 @@ app.delete('/api/notes/:id', (request, response) => {
   notes = notes.filter(note => note.id !== id)
 
   response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 const PORT = 3001

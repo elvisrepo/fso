b8137bf1be8b8e2c1d0f209207f7485b4dd3af59
elvisrepo <elvis.dev2@gmail.com>
2025-08-06 19:28:07 +0200

    refactoring

--- node_test/models/note.js
+++ node_test/models/note.js
@@ -1,26 +1,12 @@
 const mongoose = require('mongoose')
 
-
-const url = process.env.MONGODB_URI
-
-mongoose.set('strictQuery', false)
-console.log('connecting to', url)
-
-mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
-
 const noteSchema = new mongoose.Schema({
     content: {
         type: String,
-        minLength: 5,
-        required: true
+        required: true,
+        minlength: 5
     },
-    important: Boolean
+    important: Boolean,
 })
 
 noteSchema.set('toJSON', {
@@ -30,6 +16,5 @@ noteSchema.set('toJSON', {
         delete returnedObject.__v
     }
 })
-
 
 module.exports = mongoose.model('Note', noteSchema)

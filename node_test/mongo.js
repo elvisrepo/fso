3587a41fae323e424c24ff5565739d029cd7ee1f
elvisrepo <elvis.dev2@gmail.com>
2025-08-06 19:53:08 +0200

    supertest to test the api calls

--- node_test/mongo.js
+++ node_test/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:fullstack1@cluster0.vs1ziji.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://fullstack:fullstack1@cluster0.vs1ziji.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 
@@ -21,21 +21,21 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-// const note = new Note({
-//     content: 'rob is a r',
-//     important: true,
-// })
+const note = new Note({
+    content: 'bob is a f',
+    important: true,
+})
 
-// note.save().then(result => {
-//     console.log(result)
-//     mongoose.connection.close()
-// })
-
-
-Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
+note.save().then(result => {
+    console.log(result)
     mongoose.connection.close()
 })
+
+
+// Note.find({}).then(result => {
+//     result.forEach(note => {
+//         console.log(note)
+//     })
+//     mongoose.connection.close()
+// })
 

ca8d27d99dfbb30a6db142f33d87f88715ec08aa
elvisrepo <elvis.dev2@gmail.com>
2025-08-07 15:40:31 +0200

    test

---
+++ introdemo/src/components/Note.test.jsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react'
+import Note from './Note'
+
+test('renders content', () => {
+    const note = {
+        content: 'Component testing is done with react-testing-library',
+        important: true
+    }
+
+    render(<Note note={note} />)
+
+    // screen.debug()
+
+    const element = screen.getByText('Component testing is done with react-testing-library')
+    expect(element).toBeDefined()
+})
+
+
+//  const { container } = render(<Note note={note} />)
+
+//   const div = container.querySelector('.note')
+//   expect(div).toHaveTextContent(
+//     'Component testing is done with react-testing-library'
+//   )

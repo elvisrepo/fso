46db3d03a75fe06a308ff7f1c097f2dbd9270012
elvisrepo <elvis.dev2@gmail.com>
2025-08-09 13:03:01 +0200

    react thunk

--- counter/src/components/NewNote.jsx
+++ counter/src/components/NewNote.jsx
@@ -1,7 +1,6 @@
 import { useDispatch } from 'react-redux'
 import { createNote } from '../reducers/noteReducer'
-import noteService from '../services/notes'
-import { createNote } from '../reducers/noteReducer'
+
 
 const NewNote = () => {
 
--- counter/src/reducers/noteReducer.js
+++ counter/src/reducers/noteReducer.js
@@ -2,21 +2,18 @@ import { createSlice, current } from '@reduxjs/toolkit'
 
 import noteService from '../services/notes'
 
-const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
-
 
 const noteSlice = createSlice({
   name: 'notes',
-  initialState : [],
+  initialState: [],
   reducers: {
-  
+
     toggleImportanceOf(state, action) {
       const id = action.payload
       const noteToChange = state.find(n => n.id === id)
-      const changedNote = { 
-        ...noteToChange, 
-        important: !noteToChange.important 
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
       }
 
 
@@ -24,8 +21,8 @@ const noteSlice = createSlice({
 
 
       return state.map(note =>
-        note.id !== id ? note : changedNote 
-      )     
+        note.id !== id ? note : changedNote
+      )
     },
     appendNote(state, action) {
       state.push(action.payload)
@@ -47,12 +44,44 @@ export const initializeNotes = () => {
 }
 
 export const createNote = content => {
-  return async dispatch => {
+  // Inside your thunk function:
+  return async (dispatch) => {
+    // 1. Make API call
     const newNote = await noteService.createNew(content)
-    dispatch(appendNote(newNote))
+    // 2. When ready, dispatch a regular action
+    dispatch(appendNote(newNote)) // This is a plain object action!
   }
 }
 
 
 
 export default noteSlice.reducer
+
+
+
+/*
+
+The Simple Truth About Redux Thunk
+Without Thunk:
+
+// Wait for API call in component
+const newNote = await noteService.createNew(content)
+// Then dispatch regular action
+dispatch(createNote(newNote))
+
+
+With Thunk:
+// Just dispatch - the waiting happens inside the action creator
+dispatch(createNote(content))
+
+// Inside the thunk:
+const createNote = (content) => {
+  return async (dispatch) => {
+    const newNote = await noteService.createNew(content) // Wait here instead
+    dispatch(appendNote(newNote))
+  }
+}
+
+
+It's Really Just Moving the await
+*/
